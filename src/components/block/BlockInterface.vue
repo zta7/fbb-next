0baa1b88abe81653a65acae704c58aab98e2fdc9
ZTA <zta538218@gmail.com>
2022-07-11 23:30:58 +0800

    f

--- src/components/block/BlockInterface.vue
+++ src/components/block/BlockInterface.vue
@@ -5,9 +5,30 @@ const props = defineProps({
   target: Object,
 })
 const { target } = toRefs(props)
+console.log(target)
+
+const asyncFn1 = () => new Promise((res) => {
+  res(1)
+})
+
+const asyncFn2 = () => new Promise((res) => {
+  res(1)
+})
+
+const fn = () => {
+  console.log(asyncFn1())
+  console.log(asyncFn2())
+  setTimeout(() => {
+    console.log(4)
+  }, 0)
+  console.log(3)
+}
+
+fn()
+
 </script>
 <template>
   <q-scroll-area class="full-height">
-    {{ target }}
+    123
   </q-scroll-area>
 </template>
--- src/components/block/BlockPanel.vue
+++ src/components/block/BlockPanel.vue
@@ -1,53 +1,222 @@
 <script setup>
-import anime from 'animejs'
-import { onMounted, ref } from 'vue'
+// import anime from 'animejs'
+import { ref, watchEffect } from 'vue'
 
-const animation = ref(null)
+// const animation = ref(null)
 
-onMounted(() => {
-  animation.value = anime({
-    targets: '.container .box',
-    translateX: 270,
-    // direction: 'alternate',
-    autoplay: false,
-    easing: 'easeInOutSine',
-  })
+const imgs = ref([
+  'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg',
+  'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg', 'http://p1.music.126.net/I1ov-NqsABjsdRkiAs9hZg==/109951167551824032.jpg',
+  'http://p1.music.126.net/u3oq-WCH9xLEusEihNviSA==/109951167551773624.jpg',
+  'http://p1.music.126.net/393c3Mu6_Gxvs1g9s3orfg==/109951167551722650.jpg',
+  'http://p1.music.126.net/3NoXNKJqfrNS-HAuSpnTfA==/109951167552068501.jpg',
+  'http://p1.music.126.net/yZBB7zaA36MAWneoyXItYg==/109951167551751589.jpg',
+  'http://p1.music.126.net/tIYOCIa6yGhS-mCWsZ45IQ==/109951167551750897.jpg',
+])
+
+const index = ref(0)
+const items = ref([])
+
+watchEffect(() => {
+  items.value = [...imgs.value.slice(index.value, index.value + 3)]
 })
 
 const onClick = () => {
-  animation.value.play()
+  index.value += 1
 }
 </script>
 <template>
-  <div
-    class="container"
+  <TransitionGroup
+    name="fade"
+    tag="div"
+    class="container row no-wrap items-center relative-position"
     @click="onClick()">
-    <div class="box bg-red" />
-    <div class="box bg-blue" />
-    <div class="box bg-orange" />
-  </div>
+    <img
+      v-for="(e, i) in items"
+      :key="e"
+      :src="e"
+      :class="`box-${i}`"
+      class="box">
+  </TransitionGroup>
 </template>
 <style lang="scss" scoped>
   .container {
     // width: 400px;
-    position: relative;
   }
   .box {
     width: 300px;
-    position: absolute;
   }
-  .box:nth-child(1) {
-    left: 0px;
-    top: 25px;
+
+  .box-0{
     height: 150px;
   }
-  .box:nth-child(2) {
-    left: 300px;
+  .box-1 {
     height: 200px;
+    z-index: 99
   }
-  .box:nth-child(3) {
-    left: 600px;
-    top: 25px;
+  .box-2 {
     height: 150px;
   }
+
+.fade-move {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+/* 2. declare enter from and leave to state */
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: scaleY(0.01) translate(30px, 0);
+}
+
+/* 3. ensure leaving items are taken out of layout flow so that moving
+      animations can be calculated correctly. */
+.fade-leave-active {
+  position: absolute;
+}
 </style>
